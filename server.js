3069f5423cbc5aa78af510be4bd3c02445054989
Elena Perers <elena.perers@chasacademy.se>
2019-10-27 20:35:31 +0100

    Added server

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const Products = require('./models/products');
+const dotenv = require('dotenv');
+const stock = require("./stock.js");
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+var mongoose = require('mongoose');
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb://localhost:27017/trading');
+
+
+dotenv.config()
+
+
+if (process.env.NODE_ENV === "production") {
+    var url = "https://elenaperers.me";
+    var urlPort = 'https://elenaperers.me:443';
+} else {
+    var url = "http://localhost";
+    var urlPort = "http://localhost:3000";
+}
+
+app.use(cors(
+    {
+        origin: url
+    }));
+
+io.origins([urlPort]);
+
+async function getProducts() {
+    Products.find().then(function (doc) {
+        return doc;
+    });
+}
+Products.find().then(function (products) {
+    setInterval(function () {
+        products.map((product) => {
+            product["startingPoint"] = stock.getStockPrice(product);
+            return product;
+        });
+
+        io.emit("stocks", products);
+    }, 5000);
+
+});
+
+
+io.on('connection', (socket) => {
+    socket.on('SEND_MESSAGE', function (data) {
+        console.log('a user connected');
+        socket.on('disconnect', function () {
+            console.log('user disconnected');
+        });
+    })
+
+});
+
+
+
+server.listen(3005, function (err) {
+    if (err) throw err
+    console.log('listening on port 3005')
+})
